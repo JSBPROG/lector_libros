f884d901a26833f9f7b8c77a7c1c57f6d3073160
Jorge SB <167673003+JSBPROG@users.noreply.github.com>
2025-09-13 09:33:20 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,5 @@
 import os
 
 def create_directories(directories: list[str]) -> None:
-    """
-    Creates a list of directories if they don't already exist.
-    """
     for directory in directories:
         os.makedirs(directory, exist_ok=True)
